aac006c6ea4a31e25bf432d6c2412f73e7befd63
Oliver Wehrens <oliver.wehrens@epost-dev.de>
2015-08-14 11:42:28 +0200

    store locations of users in a map

--- src/main/java/net/wehrens/geofence/GeoFenceController.java
+++ src/main/java/net/wehrens/geofence/GeoFenceController.java
@@ -2,27 +2,34 @@ package net.wehrens.geofence;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class GeoFenceController {
 
+    Map<String, String> userLocations = new HashMap<>();
+
     @RequestMapping(value = "/location/{location}/in", method = RequestMethod.POST)
     public void locationIn(@PathVariable String location) {
-        System.out.println("In Location: "+location);
-        User userSession = (User) SecurityContextHolder.getContext()
-                .getAuthentication().getPrincipal();
-        System.out.println(userSession.getUsername());
+        userLocations.put(getUserName(), location);
+    }
+
+    private String getUserName() {
+        return ((User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal()).getUsername();
     }
 
     @RequestMapping(value = "/location/{location}/out", method = RequestMethod.POST)
     public void locationOut(@PathVariable String location) {
-        System.out.println("Out Location: "+location);
-        User userSession = (User) SecurityContextHolder.getContext()
-                .getAuthentication().getPrincipal();
-        System.out.println(userSession.getUsername());
+        if (userLocations.containsKey(getUserName())) {
+            userLocations.remove(getUserName());
+        }
     }
 
 
