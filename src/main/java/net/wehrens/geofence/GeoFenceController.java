daf3312c1a4c9cc9718809897935b37e049393ca
Oliver Wehrens <oliver.wehrens@epost-dev.de>
2015-08-14 14:14:50 +0200

    introducing some more status codes

--- src/main/java/net/wehrens/geofence/GeoFenceController.java
+++ src/main/java/net/wehrens/geofence/GeoFenceController.java
@@ -1,13 +1,14 @@
 package net.wehrens.geofence;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,8 +16,9 @@ public class GeoFenceController {
 
     Map<String, String> userLocations = new HashMap<>();
 
-    @RequestMapping(value = "/location/{location}/in", method = RequestMethod.POST)
-    public void locationIn(@PathVariable String location) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(value = "/locations/{location}", method = RequestMethod.POST)
+    public void addUserToLocation(@PathVariable String location) {
         userLocations.put(getUserName(), location);
     }
 
@@ -25,12 +27,57 @@ public class GeoFenceController {
                 .getAuthentication().getPrincipal()).getUsername();
     }
 
-    @RequestMapping(value = "/location/{location}/out", method = RequestMethod.POST)
-    public void locationOut(@PathVariable String location) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(value = "/locations/{location}", method = RequestMethod.DELETE)
+    public void removeUserFromLocation(@PathVariable String location,  HttpServletResponse response) {
+       if (!removeUserFromLocationMap(location)) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+       }
+    }
+
+    private boolean removeUserFromLocationMap(@PathVariable String location) {
         if (userLocations.containsKey(getUserName())) {
-            userLocations.remove(getUserName());
+            if (userLocations.get(getUserName()).equals(location)) {
+                userLocations.remove(getUserName());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(value = "/locations/{location}/leave", method = RequestMethod.POST)
+    public void removeUserFromLocationForGeofancyBecauseItHasJustPOSTandGET(@PathVariable String location, HttpServletResponse response) {
+        if (!removeUserFromLocationMap(location)) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
     }
 
+    @RequestMapping(value = "/locations/{location}/{user}", method = RequestMethod.GET, produces = "application/json")
+    public void location(@PathVariable String location, @PathVariable String user, HttpServletResponse response) {
+        if (userLocations.containsKey(user)) {
+            if (userLocations.get(user).equals(location)) {
+                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            }
+        }
+        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    @RequestMapping(value = "/locations", produces = "application/json")
+    public @ResponseBody Map getLocations() {
+        return userLocations;
+    }
+
+    @RequestMapping(value = "/locations/{requestLocation}", produces = "application/json")
+    public @ResponseBody List<String> getUserForLocations(@PathVariable String requestLocation) {
+        List<String> usersAtLocation = new ArrayList<>();
+
+        userLocations.keySet().forEach(user -> {
+            if (userLocations.get(user).equals(requestLocation)) {
+                usersAtLocation.add(user);
+            }
+        });
+        return usersAtLocation;
+    }
 
 }
